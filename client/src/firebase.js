69e097be197b3daca3ee02d165b506f0292d6c25
Harsha Vardhan Narlakanti <harshanarlakanti80963@gmail.com>
2025-10-06 20:35:40 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,16 @@
 
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-
+// client/src/firebase.js
 const firebaseConfig = {
-  apiKey: "AIzaSyCWTxkgcyJrqzabhi5TjKfFD8Sr02XZl-Q",
-  authDomain: "socialex-85cb3.firebaseapp.com",
-  projectId: "socialex-85cb3",
-  storageBucket: "socialex-85cb3.appspot.com",
-  messagingSenderId: "682531867137",
-  appId: "1:682531867137:web:b9e07d8af4bcad5ba64e00"
+  apiKey: "YOUR_NEW_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "XXXXXXXXXX"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
